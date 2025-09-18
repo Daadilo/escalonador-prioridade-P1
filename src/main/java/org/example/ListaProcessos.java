9f82f56c63a7e2a084fd41524cbcbbdebcc2def7
Daadilo <danilocamposfernandes22@gmail.com>
2025-09-17 21:46:32 -0300

    Update ListaProcessos.java

--- src/main/java/org/example/ListaProcessos.java
+++ src/main/java/org/example/ListaProcessos.java
@@ -1,1 +1,68 @@
+package org.example;
 
+public class ListaProcessos {
+    private class Node {
+        Processo processo;
+        Node proximo;
+
+        Node(Processo processo) {
+            this.processo = processo;
+            this.proximo = null;
+        }
+    }
+
+    private Node cabeca;
+    private Node cauda;
+
+    public ListaProcessos() {
+        this.cabeca = null;
+        this.cauda = null;
+    }
+
+    public void adicionarFinal(Processo processo) {
+        Node novoNo = new Node(processo);
+        if (cabeca == null) {
+            cabeca = cauda = novoNo;
+        } else {
+            cauda.proximo = novoNo;
+            cauda = novoNo;
+        }
+    }
+
+    public Processo removerInicio() {
+        if (cabeca == null) return null;
+
+        Processo processo = cabeca.processo;
+        cabeca = cabeca.proximo;
+        if (cabeca == null) cauda = null;
+
+        return processo;
+    }
+
+    public boolean estaVazia() {
+        return cabeca == null;
+    }
+
+    public void percorrerLista() {
+        Node atual = cabeca;
+        if (atual == null) {
+            System.out.println("| Lista vazia");
+            return;
+        }
+        while (atual != null) {
+            System.out.println("| " + atual.processo);
+            atual = atual.proximo;
+        }
+    }
+
+    public void imprimir() {
+        Node atual = cabeca;
+        System.out.print("[");
+        while (atual != null) {
+            System.out.print(atual.processo.toString());
+            if (atual.proximo != null) System.out.print(", ");
+            atual = atual.proximo;
+        }
+        System.out.print("]");
+    }
+}
